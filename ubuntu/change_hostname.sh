bc9895c32a77e09d98a51fdfb9172a83a2d6dc4a
Haijun Su <navysu@gmail.com>
2018-06-12 01:38:28 -0400

    add cronjobs

--- ubuntu/change_hostname.sh
+++ ubuntu/change_hostname.sh
@@ -2,7 +2,8 @@
 
 #change host name
 HOST_NAME=`hostname`
-hostname ${1}
+# hostname ${1}
+hostnamectl set-hostname ${1}
 sed -i "s/${HOSTNAME}/${1}/g" /etc/hostname
 sed -i "s/${HOSTNAME}/${1}/g" /etc/hosts
 sed -i "s/${HOSTNAME}/${1}/g" /etc/postfix/main.cf
